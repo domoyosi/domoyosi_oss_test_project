9a2ed485fbe3a1a19053a9e6df3d903e6ce8b549
domoyosi <domoyosi@gmail.com>
2023-07-03 16:25:13 +0900

    init

---
+++ src/test/java/com/example/domoyosi_oss_test_project/DomoyosiOssTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.domoyosi_oss_test_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomoyosiOssTestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
